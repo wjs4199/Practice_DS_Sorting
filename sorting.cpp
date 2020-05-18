1c6c842c60944dbe5ae2878de4931bb21c59832b
wjs4199 <63443458+wjs4199@users.noreply.github.com>
2020-05-18 23:56:26 +0900

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,11 @@
+  void SelectionSort(int list[], int n) { 
+  int i = 0, j = 0, min = 0, temp = 0; 
+  for(i = 0; i < n - 1; i++)){ 
+    // find the minimum of list[i] through list[n-1] 
+    min = i; 
+    for(j = i + 1; j < n; j++){ 
+      if(list[j].key < list[min].key) min = j; 
+    } 
+    swap(&list[i], &list[min]); 
+    } 
+  } 
