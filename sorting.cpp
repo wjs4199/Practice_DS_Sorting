061297f4b6c7aa6c2cac06851d91333d6cdfa785
wjs4199 <63443458+wjs4199@users.noreply.github.com>
2020-05-19 23:55:29 +0900

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,21 +1,21 @@
-  
+#include "sorting.h"  
 
-void SelectionSort(int list[], int n) { 
+void SelectionSort(Element list[], int n) { 
   int i = 0, j = 0, min = 0, temp = 0; 
   for(i = 0; i < n - 1; i++)){ 
     // find the minimum of list[i] through list[n-1] 
     min = i; 
     for(j = i + 1; j < n; j++){ 
-      if(list[j] < list[min]) min = j; 
+      if(list[j].key < list[min].key) min = j; 
     } 
     swap(&list[i], &list[min]); 
     } 
   } 
 
-void BubbleSort(int list[], int n) { 
+void BubbleSort(Element list[], int n) { 
   int i, j; 
   for (i = 0; i < n-1; i++) 
     for (j = 0; j < n-i-1; j++) 
-      if (list[j] > list[j+1]) 
+      if (list[j].key > list[j+1].key) 
         swap(&list[j], &list[j+1]); 
 }
