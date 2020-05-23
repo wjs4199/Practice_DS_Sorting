ef962bee6f33cf0a9600c2ca79c69779260a38cf
wjs4199 <63443458+wjs4199@users.noreply.github.com>
2020-05-23 23:44:28 +0900

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -4,4 +4,17 @@ typdef struct {
 int key; /* other fields */ 
 } Element;
 
+/*prototype*/
+void SelectionSort(Element list[], int n);
 
+void BubbleSort(Element list[], int n) ;
+
+void InsertionSort(Element list[], int n);
+
+void QuickSort(Element list[], int left, int right);
+
+void merge(Element list[], Element sorted[], int i, int m, int n) ;
+
+void adjust(Element list[], int root, int n);
+
+void HeapSort(Element list[], int n);
